c3ba5a8d0d433f5b5529a8e433f17475a709714f
Joel Saltzman <talk@joelsaltzman.com>
2021-03-10 08:00:11 -0800

    Added files

---
+++ ServerTest/ServerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServerTestApp.swift
+//  ServerTest
+//
+//  Created by Xcode Server on 10/7/20.
+//
+
+import SwiftUI
+
+@main
+struct ServerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
